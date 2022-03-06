3a49b989985889a7c188398c5233f74861c3ae3f
Kimmo Kiiski <kimmo@kii.ski>
2022-03-06 20:44:30 +0200

    Show sanitized contenteditable area

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { SyntheticEvent, useRef } from "react";
+import "./App.css";
+
+const preservedNodeNames = ["DIV", "BR"];
+
+function sanitize(element: ChildNode) {
+  const parent = element.parentNode;
+  let child = element.firstChild;
+  while (child) {
+    const nextChild = child.nextSibling;
+    if (parent && element.nodeName !== "DIV") {
+      parent.insertBefore(child, element);
+    }
+    sanitize(child);
+    if (
+      child.nodeType === Node.ELEMENT_NODE &&
+      !preservedNodeNames.includes(child.nodeName)
+    ) {
+      element.removeChild(child);
+    }
+    child = nextChild;
+  }
+}
 
 function App() {
+  const contentEditableRef = useRef<HTMLDivElement>(null);
+  const onContentEditableChange = (event: SyntheticEvent<HTMLDivElement>) => {
+    sanitize(event.currentTarget);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Kalevalamitta</h1>
+        <div
+          style={{ backgroundColor: "black", minHeight: 100, minWidth: 100 }}
+          contentEditable
+          ref={contentEditableRef}
+          onInput={onContentEditableChange}
+          onBlur={onContentEditableChange}
+          onKeyUp={onContentEditableChange}
+          onKeyDown={onContentEditableChange}
+        />
       </header>
     </div>
   );
