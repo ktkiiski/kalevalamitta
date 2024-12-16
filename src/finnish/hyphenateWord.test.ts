636e754c5dd3fcb0c5c09d3453f343125f3ae7e3
Kimmo Kiiski <kimmo@kii.ski>
2024-12-16 13:08:42 +0200

    Use Vitest for tests

--- src/finnish/hyphenateWord.test.ts
+++ src/finnish/hyphenateWord.test.ts
@@ -1,3 +1,4 @@
+import { describe, test, expect } from 'vitest';
 import hyphenateWord from './hyphenateWord';
 
 const hyphenations = [
--- src/verses/parseVerses.test.ts
+++ src/verses/parseVerses.test.ts
@@ -1,3 +1,4 @@
+import { describe, test, expect } from 'vitest';
 import hyphenateText, { Token } from '../finnish/hyphenateText';
 import parseVerses from './parseVerses';
 
