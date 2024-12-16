6b31c5de9b9b1b333c673c36d90dcaf7e633e96f
Kimmo Kiiski <kimmo@kii.ski>
2024-12-16 13:13:10 +0200

    Cleanup remaining stuff from CRA

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'normalize.css/normalize.css';
 import './index.css';
 
@@ -19,8 +18,3 @@ renderApp(
   </React.StrictMode>,
   document.getElementById('root')!,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
